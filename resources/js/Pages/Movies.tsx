e5ae33e513d2c5e053358acd3d3883c42db0c575
Silovisk <silasaraujo1@hotmail.com>
2023-11-21 13:35:16 -0300

    first commit

---
+++ resources/js/Pages/Movies.tsx
@@ -0,0 +1,10 @@
+import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout";
+import { Head } from "@inertiajs/react";
+import { PageProps } from "@/types";
+import { useState } from "react";
+
+const Movies = ({ auth }: PageProps) => {
+    return <></>;
+}
+
+export default Movies;
